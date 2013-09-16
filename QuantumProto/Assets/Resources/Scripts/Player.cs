ae6bc03d9927291c3b50235930e2e2748ad6c7ad
russelljahn <russelljahn@gmail.com>
2013-09-16 17:21:02 -0500

    Minor changes to test scene. Added Player script for Player GameObject.

--- QuantumProto/Assets/Resources/Scripts/Player.cs
+++ QuantumProto/Assets/Resources/Scripts/Player.cs
@@ -5,11 +5,37 @@ public class Player : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+		Debug.Log ("Initializing player...");
+		
+		
+		// ORTHELLO EXAMPLE CODE
+		
+		// Lookup this block's sprite
+	    OTSprite sprite = GetComponent<OTSprite>();
+		sprite.name = "Player";
+	    // Set this sprite's collision delegate 
+	    // HINT : We could use sprite.InitCallBacks(this) as well.
+	    // but because delegates are the C# way we will use this technique
+	    sprite.onCollision = OnCollision;  
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+	}
+	
+	// This method will be called when this block is hit.
+	public void OnCollision(OTObject owner) {
+		
+		Debug.Log (owner.name + " is colliding with something!");
+		
+		
+		
+		// ORTHELLO EXAMPLE CODE
+		
+	    // Set color fading indicator
+	    //colorFade = true;
+	    // Reset fade time
+	    //fadeTime = 0;
 	}
 }
