b0b7e6be590c6e5be1bcefd2a1ba3ac2c2ff0d8d
russelljahn <russelljahn@gmail.com>
2013-09-17 15:43:10 -0500

    Added expanded but unfinished version of the player script. Added a few more test scenes.

--- QuantumProto/Assets/Resources/Scripts/Player.cs
+++ QuantumProto/Assets/Resources/Scripts/Player.cs
@@ -5,16 +5,38 @@ public class Player : MonoBehaviour {
 	
 	#region public variables
 		public float walkingVelocity = 1000.0f;
+		public float jumpHeight = 500.0f;
 	#endregion
 	
 	#region private variables
-		OTSprite playerSprite;
+		private OTSprite playerSprite;
+	
+		public enum playerStates { // public only for debugging
+			STANDING,
+			WALKING,
+			JUMPING
+		}
+	
+		public enum playerDirections { // public only for debugging
+			LEFT,
+			RIGHT
+		}
+	
+		public playerStates currentState; // public only for debugging
+		public playerDirections currentDirection; // public only for debugging
+		public bool currentlyJumping;
 	#endregion
 	
+	
+
 	
 	// Use this for initialization
 	void Start () {
 		Debug.Log ("Initializing player...");
+		
+		currentState = playerStates.STANDING;
+		currentDirection = playerDirections.LEFT;
+		currentlyJumping = false;
 		
 		
 		// ORTHELLO EXAMPLE CODE
@@ -30,14 +52,68 @@ public class Player : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		// Update direction
+		UpdateDirection();
+		UpdateState();
+		Move();
+		// Update state
+		// move player based on state
+	
+	}
+	
+	
+	void UpdateDirection() {
+		// Handle changing directions right to left
 		if (Input.GetKey("left")) {
-			this.transform.Translate( new Vector2(-1 * walkingVelocity * Time.deltaTime, 0) );
+			if (currentDirection == playerDirections.RIGHT) {
+				currentDirection = playerDirections.LEFT;	
+			}
+		}
+		// Handle changing directions left to right
+		else if (Input.GetKey("right")) {
+			if (currentDirection == playerDirections.LEFT) {
+				currentDirection = playerDirections.RIGHT;	
+			}
+		}	
+	}
+	
+	
+	void UpdateState() {
+		// If in midair, ignore. state will be changed from jumping in collision detection. 
+		if (currentlyJumping) {
+			return;	
+		}
+		else if (Input.GetKey("left")) {
+			currentState = playerStates.WALKING;
 		}
 		else if (Input.GetKey("right")) {
-			this.transform.Translate( new Vector2(walkingVelocity * Time.deltaTime, 0) );
+			currentState = playerStates.WALKING;
 		}
+		else if (Input.GetButton("Jump")) {
+			currentState = playerStates.JUMPING;
+		}
+		else {
+			currentState = playerStates.STANDING;	
+		}
+	}
 	
+	
+	void Move() {
+		if (currentlyJumping) {
+			return;
+		}
+		else if (Input.GetKey("left")) {
+			// if player should walk left
+			if (currentState == playerStates.WALKING) {
+				this.transform.Translate( new Vector2(-1 * walkingVelocity * Time.deltaTime, 0) );
+			}
+		}
+		else if (Input.GetKey("right")) {
+			// if player should walk right
+			if (currentState == playerStates.WALKING) {
+				this.transform.Translate( new Vector2(walkingVelocity * Time.deltaTime, 0) );
+			}
+		}	
 	}
 	
 	// This method will be called when this block is hit.
@@ -45,13 +121,11 @@ public class Player : MonoBehaviour {
 		
 		Debug.Log (owner.name + " is colliding with something!");
 		
+		// If landing from a jump
+		if (currentState == playerStates.JUMPING) {
+			currentState = playerStates.STANDING;	
+			currentlyJumping = false;
+		}
 		
-		
-		// ORTHELLO EXAMPLE CODE
-		
-	    // Set color fading indicator
-	    //colorFade = true;
-	    // Reset fade time
-	    //fadeTime = 0;
 	}
 }
---
+++ QuantumProto/Assets/Resources/Scripts/Tilemap.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tilemap : MonoBehaviour {
+	
+	private OTTileMap tilemap;
+	
+	// Use this for initialization
+	void Start () {
+		tilemap = this.GetComponent<OTTileMap>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
