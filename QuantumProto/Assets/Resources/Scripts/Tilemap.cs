67a380126d78322fb7c047602f9d06004321b745
russelljahn <russelljahn@gmail.com>
2013-09-17 22:49:39 -0500

    Readding missing assets.

---
+++ QuantumProto/Assets/Resources/Scripts/Tilemap.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tilemap : MonoBehaviour {
+	
+	private OTTileMap tilemap;
+	
+	// Use this for initialization
+	void Start () {
+		tilemap = this.GetComponent<OTTileMap>();
+		
+		tilemap.generateColliders = false;
+		BuildColliders();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	void BuildColliders() {
+
+	
+	
+	}
+}
