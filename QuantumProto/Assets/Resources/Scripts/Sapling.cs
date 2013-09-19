e1a20cf5e4665a137f18ddae43f6dccb9a3db3e0
rllamas <ricardo.llamas11@yahoo.com>
2013-09-18 19:59:44 -0500

    Added pickup items, simplified portal script

--- QuantumProto/Assets/Resources/Scripts/Sapling.cs
+++ QuantumProto/Assets/Resources/Scripts/Sapling.cs
@@ -2,9 +2,16 @@
 using System.Collections;
 
 public class Sapling : MonoBehaviour {
-
+	
+	#region private variables
+	private bool inTrigger = false;
+	
+	#endregion
+	
 	// Use this for initialization
 	void Start () {
+		collider.isTrigger = true;
+		
 		
 		// Don't allow rotations
 		rigidbody.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationY | 
@@ -13,6 +20,21 @@ public class Sapling : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		collider.isTrigger = true;
+		
+		if (inTrigger && Input.GetButton("Fire1")){
+			Debug.Log ("Boom.");
+			Destroy(this.gameObject);
+		}
+	}
 	
+	void OnTriggerEnter(Collider other) {
+		Debug.Log ("In range.");
+		inTrigger = true;
+    }
+	
+	void OnTriggerExit(Collider other) {
+		Debug.Log ("Out of range.");
+		inTrigger = false;
 	}
 }
