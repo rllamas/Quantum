dbfdcef7867871df73d0f43dbeb656380464f7a7
russelljahn <russelljahn@gmail.com>
2013-09-22 16:59:38 -0500

    Wrote code to get the next Player direction every frame.

--- QuantumProto_TK2D/Assets/Scripts/Player.cs
+++ QuantumProto_TK2D/Assets/Scripts/Player.cs
@@ -1,19 +1,105 @@
 ﻿using UnityEngine;
+using System;
 using System.Collections;
+using Quantum.States;
 
 public class Player : MonoBehaviour {
 	
-	#region private members
-		public GameState currentState; // Only public for debugging!
-	#endregion
 	
-	// Use this for initialization
+	/* All possible directions the player can be in. */
+	public enum Direction {
+		LEFT,
+		RIGHT,
+		TURNING_LEFT_TO_RIGHT,
+		TURNING_RIGHT_TO_LEFT,
+		NONE,
+	};
+	
+	
+	
+	/* Current game state that the player is in. */
+	private GameState currentState;
+	public GameState CurrentState {
+		get {
+			return currentState;
+		}
+	}
+	
+	
+	
+	/* Current direction that the player is in. */
+	private Direction currentDirection;
+	public Direction CurrentDirection {
+		get {
+			return currentDirection;
+		}
+	}
+	
+	
+	
+	/* Get the next direction that the player should */
+	private Direction NextDirection() { 
+		
+		float horizontalAxis = Input.GetAxis("Horizontal");
+		Direction nextDirection;
+		
+		/* If the player isn't pressing left or right. */
+		if (horizontalAxis == 0) {
+			/* If the character is in the middle of turning left to right. */
+			if (currentDirection == Direction.TURNING_LEFT_TO_RIGHT) {
+				nextDirection = Direction.RIGHT;	
+			}
+			/* If the character is in the middle of turning right to left. */
+			else if (currentDirection == Direction.TURNING_RIGHT_TO_LEFT) {
+				nextDirection = Direction.LEFT;	
+			}
+			/* Otherwise, remain in the same direction as the last update. */
+			else {
+				nextDirection = currentDirection;
+			}
+		}
+		/* If the player is pressing left. */
+		else if (horizontalAxis < 0) {
+			/* If last direction was right and currently going left, mark as turning right to left. */
+			if (currentDirection == Direction.RIGHT) {
+				nextDirection = Direction.TURNING_RIGHT_TO_LEFT;	
+			}
+			else {
+				nextDirection = Direction.LEFT;	
+			}
+		}
+		/* If the player is pressing right. */
+		else if (horizontalAxis > 0) {
+			/* If last direction was left and currently going right, mark as turning left to right. */
+			if (currentDirection == Direction.LEFT) {
+				nextDirection = Direction.TURNING_LEFT_TO_RIGHT;	
+			}
+			else {
+				nextDirection = Direction.RIGHT;	
+			}
+		}
+		/* If you hit here, you probably fucked up somewhere. */
+		else {
+			throw new Exception("Invalid direction state!");	
+		}
+		return nextDirection;
+
+	} // end NextDirection()
+	
+	
+	
 	void Start () {
-	
+		currentDirection = Direction.LEFT;
+		currentState = new ProfessorStandingState();
 	}
 	
-	// Update is called once per frame
+	
+	
 	void Update () {
-	
+		currentDirection = NextDirection();
+		currentState = currentState.NextState();
+		currentState.Logic();
 	}
+	
+	
 }
