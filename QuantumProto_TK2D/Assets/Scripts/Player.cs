eab7242d90cfa3ec813c39b24fc926d80cd1b31d
russelljahn <russelljahn@gmail.com>
2013-09-22 15:54:36 -0500

    Changed folder structure.

---
+++ QuantumProto_TK2D/Assets/Scripts/Player.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+	
+	#region private members
+		public GameState currentState; // Only public for debugging!
+	#endregion
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
