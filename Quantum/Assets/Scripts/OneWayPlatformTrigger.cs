9b466820c955b406fbb3e38bde9e4c54f25f4c62
russelljahn <russelljahn@gmail.com>
2013-10-02 13:42:53 -0500

    Added beanstalk sprites for plants, and implemented plants changing based on the time era in the Prototype scene..

---
+++ Quantum/Assets/Scripts/OneWayPlatformTrigger.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OneWayPlatformTrigger : MonoBehaviour {
+	
+	/*
+	 The colliders should be placed like this, with this trigger under the platform,
+	 but NOT COMPLETELY BOUNDING the platform.
+	 
+	 When the player jumps through this trigger, the platform's collider will be disabled, and when the
+	 player is above this trigger, the platform's collider will be re-enabled.
+	   ___________
+	  | platform |
+	---------------
+	| |_________| |
+	|   trigger   |
+	---------------
+			
+	*/
+	
+	/* The platform for the player to step on. This should be a non-trigger BoxCollider. */
+	public BoxCollider platform;	
+	
+	private Player player;
+	
+	
+	
+	
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindWithTag("Player").GetComponent<Player>();
+	}
+	
+	
+	
+	
+	void OnTriggerEnter() {
+		Physics.IgnoreCollision(player.gameObject.collider, platform.collider, true);
+	}
+	
+	
+	
+	
+	void OnTriggerExit() {
+		Physics.IgnoreCollision(player.gameObject.collider, platform.collider, false);
+	}
+	
+	
+	
+	
+}
---
+++ Quantum/Assets/Scripts/Pickups/Plant.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Plant : Pickup {
+	
+	public GameObject pastPlant; // Plant object used in the past era.
+	public GameObject futurePlant; // Plant object use in the future era.
+		
+	
+	
+	
+	public override void Start() {
+		base.Start();
+		pastPlant.transform.localPosition = Vector3.zero;
+		futurePlant.transform.localPosition = Vector3.zero;
+		
+		HandleEra();
+	}
+	
+	
+	
+	
+	public override void Update() {
+		base.Update();
+		HandleEra();
+	}
+	
+	
+	
+	
+	/* Sets the plant object being used based on the time era. */
+	private void HandleEra() {
+		if (Vortex.isPast) {
+			pastPlant.SetActive(true);	
+			futurePlant.SetActive(false);	
+		}
+		else {
+			pastPlant.SetActive(false);	
+			futurePlant.SetActive(true);	
+		}
+	}
+		
+	
+	
+
+}
