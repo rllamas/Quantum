24336db204251536641719b8e23b7fb2bdbcf4ae
russelljahn <russelljahn@gmail.com>
2013-10-03 18:49:54 -0500

    Refactored collider generation script.

--- Quantum/Assets/Scripts/TileMaps/GenerateTilemapColliders.cs
+++ Quantum/Assets/Scripts/TileMaps/GenerateTilemapColliders.cs
@@ -24,6 +24,7 @@ public class GenerateTilemapColliders : MonoBehaviour {
 	
 	
 	void Start () {
+		
 		tilemap = GetComponent<tk2dTileMap>();
 		spriteDefinitions = tilemap.SpriteCollectionInst.spriteDefinitions;
 		
@@ -34,6 +35,7 @@ public class GenerateTilemapColliders : MonoBehaviour {
 		maxTilesX = Mathf.Min(tilemap.partitionSizeX, tilemap.width);
 		
 		GenerateColliders();
+	
 	}
 	
 	
@@ -43,56 +45,60 @@ public class GenerateTilemapColliders : MonoBehaviour {
 	 * set for their sprites. */
 	void GenerateColliders() {
 		
-			int numberOfLayers = tilemap.Layers.Length;
+		int numberOfLayers = tilemap.Layers.Length;
 
-			/* Create an container gameObject to parent all layers of box colliders.
-			 * This helps dramatically with scene organization. */
-			GameObject boxCollidersContainer = new GameObject();
-			boxCollidersContainer.name = "Box Colliders Container";
-			boxCollidersContainer.transform.position = new Vector3(
-				tilemap.transform.position.x,
-				tilemap.transform.position.y,
-				tilemap.transform.position.z
-			);
-		
-			/* For every layer... */
-			for (int layerID = 0; layerID < numberOfLayers; ++layerID) {
-			
-				Layer currentLayer = tilemap.Layers[layerID];
-			
-				/* Add a layer gameObject to parent all box colliders in this layer.
-				 * This helps dramatically with scene organization. */
-				GameObject currentLayerContainer = new GameObject();
-				currentLayerContainer.name = "Layer " + layerID;
-				currentLayerContainer.transform.position = boxCollidersContainer.transform.position;
-				currentLayerContainer.transform.parent = boxCollidersContainer.transform;
-			
-				/* Ignore layer if it's empty or set to not generate colliders. */
-				if (currentLayer.IsEmpty || !tilemap.data.Layers[layerID].generateCollider) {
-					continue;
-				}
+		/* Create an container gameObject to parent all layers of box colliders.
+		 * This helps dramatically with scene organization. */
+		GameObject boxCollidersContainer = new GameObject();
+		boxCollidersContainer.name = "Box Colliders Container";
+		boxCollidersContainer.transform.position = new Vector3(
+			tilemap.transform.position.x,
+			tilemap.transform.position.y,
+			tilemap.transform.position.z
+		);
 	
-				/* For every row of sprite chunks... */
-				for (int chunkY = 0; chunkY < currentLayer.numRows; ++chunkY) {
-				
-					/* For every column of sprite chunks... */
-					for (int chunkX = 0; chunkX < currentLayer.numColumns; ++chunkX) {
-					
-						/* Add a chunk gameObject to parent all box colliders in this chunk.
-						 * This helps dramatically with scene organization. */
-						GameObject currentChunkContainer = new GameObject();
-						currentChunkContainer.name = "Chunk " + chunkX + " " + chunkY;
-						currentChunkContainer.transform.position = boxCollidersContainer.transform.position;
-						currentChunkContainer.transform.parent = currentLayerContainer.transform;
-				
-						SpriteChunk chunk = currentLayer.GetChunk(chunkX, chunkY);
-						GenerateCollidersForChunk(chunk, currentChunkContainer);		
-					
-					}
-				
-				} 
+		/* For every layer... */
+		for (int layerID = 0; layerID < numberOfLayers; ++layerID) {
 			
+			
+			Layer currentLayer = tilemap.Layers[layerID];
+			
+			
+			/* Add a layer gameObject to parent all box colliders in this layer.
+			 * This helps dramatically with scene organization. */
+			GameObject currentLayerContainer = new GameObject();
+			currentLayerContainer.name = "Layer " + layerID;
+			currentLayerContainer.transform.position = boxCollidersContainer.transform.position;
+			currentLayerContainer.transform.parent = boxCollidersContainer.transform;
+		
+			
+			/* Ignore layer if it's empty or set to not generate colliders. */
+			if (currentLayer.IsEmpty || !tilemap.data.Layers[layerID].generateCollider) {
+				continue;
 			}
+			
+
+			/* For every row of sprite chunks... */
+			for (int chunkY = 0; chunkY < currentLayer.numRows; ++chunkY) {
+			
+				/* For every column of sprite chunks... */
+				for (int chunkX = 0; chunkX < currentLayer.numColumns; ++chunkX) {
+				
+					/* Add a chunk gameObject to parent all box colliders in this chunk.
+					 * This helps dramatically with scene organization. */
+					GameObject currentChunkContainer = new GameObject();
+					currentChunkContainer.name = "Chunk " + chunkX + " " + chunkY;
+					currentChunkContainer.transform.position = boxCollidersContainer.transform.position;
+					currentChunkContainer.transform.parent = currentLayerContainer.transform;
+			
+					SpriteChunk chunk = currentLayer.GetChunk(chunkX, chunkY);
+					GenerateCollidersForChunk(chunk, currentChunkContainer);		
+				
+				}
+			
+			} 
+		
+		}
 	} // GenerateColliders
 	
 	
@@ -101,18 +107,23 @@ public class GenerateTilemapColliders : MonoBehaviour {
 	/* Creates Farseer Box Colliders for tiles in chunk and sets them as children of container. */
 	void GenerateCollidersForChunk(SpriteChunk chunk, GameObject container) {
 		
+		
 		int spriteCount = spriteDefinitions.Length;
 		int[] chunkData = chunk.spriteIds;
+		
 		
 		/* For every row in the chunk... */
 		for (int y = 0; y < maxTilesY; ++y) {
 		
+			
 			/* For every column in the chunk... */
 			for (int x = 0; x < maxTilesX; ++x) {
 	
+				
 				int spriteId = chunkData[y * tilemap.partitionSizeX + x];
 				int spriteIdx = BuilderUtil.GetTileFromRawTile(spriteId);
 			
+				
 				/* Handle if tile has no valid sprite. */
 				if (spriteIdx < 0 || spriteIdx >= spriteCount) {
 					continue;
@@ -121,8 +132,10 @@ public class GenerateTilemapColliders : MonoBehaviour {
 					continue;
 				}
 				
+				
 				tk2dSpriteDefinition spriteData = spriteDefinitions[spriteIdx];
 			
+				
 				/* If the tile is marked as User Defined, then add a box collider. */
 				if (spriteData.colliderType == tk2dSpriteDefinition.ColliderType.Unset) {
 			
@@ -139,6 +152,8 @@ public class GenerateTilemapColliders : MonoBehaviour {
 				}
 			}	
 		}
+		
+		
 	} // End method GenerateCollidersForChunk
 	
 	
@@ -148,70 +163,71 @@ public class GenerateTilemapColliders : MonoBehaviour {
 	GameObject CreateBoxCollider(Vector3 position, string newGameObjectName) {
 		
 		
-			/* Generate new GameObject with Box Collider to return. */
-			GameObject newColliderGameObject = new GameObject();
-			newColliderGameObject.name = newGameObjectName;
-			newColliderGameObject.transform.position = position;
-			newColliderGameObject.AddComponent<FSShapeComponent>();
+		/* Generate new GameObject with Box Collider to return. */
+		GameObject newColliderGameObject = new GameObject();
+		newColliderGameObject.name = newGameObjectName;
+		newColliderGameObject.transform.position = position;
+		newColliderGameObject.AddComponent<FSShapeComponent>();
+	
 		
-			FSShapeComponent boxCollider = newColliderGameObject.GetComponent<FSShapeComponent>();
-			boxCollider.SType = FarseerPhysics.Collision.Shapes.ShapeType.Polygon;
+		FSShapeComponent boxCollider = newColliderGameObject.GetComponent<FSShapeComponent>();
+		boxCollider.SType = FarseerPhysics.Collision.Shapes.ShapeType.Polygon;
+	
+	
+		/* Generate the positions of the points of the Box Collider. */
+		Vector3 positionBottomLeft = new Vector3(
+			position.x - 0.5f*tileWidth,
+			position.y - 0.5f*tileHeight,
+			position.z
+		);
+	
+		Vector3 positionBottomRight = new Vector3(
+			position.x + 0.5f*tileWidth,
+			position.y - 0.5f*tileHeight,
+			position.z
+		);
+	
+		Vector3 positionTopRight = new Vector3(
+			position.x + 0.5f*tileWidth,
+			position.y + 0.5f*tileHeight,
+			position.z
+		);
+	
+		Vector3 positionTopLeft = new Vector3(
+			position.x - 0.5f*tileWidth,
+			position.y + 0.5f*tileHeight,
+			position.z
+		);
+	
 		
-		
-			/* Generate the positions of the points of the Box Collider. */
-			Vector3 positionBottomLeft = new Vector3(
-				position.x - 0.5f*tileWidth,
-				position.y - 0.5f*tileHeight,
-				position.z
-			);
-		
-			Vector3 positionBottomRight = new Vector3(
-				position.x + 0.5f*tileWidth,
-				position.y - 0.5f*tileHeight,
-				position.z
-			);
-		
-			Vector3 positionTopRight = new Vector3(
-				position.x + 0.5f*tileWidth,
-				position.y + 0.5f*tileHeight,
-				position.z
-			);
-		
-			Vector3 positionTopLeft = new Vector3(
-				position.x - 0.5f*tileWidth,
-				position.y + 0.5f*tileHeight,
-				position.z
-			);
-		
-			
-			/* Generate GameObjects to be used as the points of the new Box Collider. */
-			GameObject pointBottomLeft = new GameObject();
-			pointBottomLeft.name = newGameObjectName + " Bottom Left Point";
-			pointBottomLeft.transform.position = positionBottomLeft;
-			pointBottomLeft.transform.parent = newColliderGameObject.transform;
-		
-			GameObject pointBottomRight = new GameObject();
-			pointBottomRight.name = newGameObjectName + " Bottom Right Point";
-			pointBottomRight.transform.position = positionBottomRight;
-			pointBottomRight.transform.parent = newColliderGameObject.transform;
-		
-			GameObject pointTopRight = new GameObject();
-			pointTopRight.name = newGameObjectName + " Top Right Point";
-			pointTopRight.transform.position = positionTopRight;
-			pointTopRight.transform.parent = newColliderGameObject.transform;
-		
-			GameObject pointTopLeft = new GameObject();
-			pointTopLeft.name = newGameObjectName + " Top Left Point";
-			pointTopLeft.transform.position = positionTopLeft;
-			pointTopLeft.transform.parent = newColliderGameObject.transform;
-		
-		
-			boxCollider.PolygonPoints = new Transform [] {
-				pointBottomLeft.transform,
-				pointBottomRight.transform,
-				pointTopRight.transform,
-				pointTopLeft.transform
-			};
+		/* Generate GameObjects to be used as the points of the new Box Collider. */
+		GameObject pointBottomLeft = new GameObject();
+		pointBottomLeft.name = newGameObjectName + " Bottom Left Point";
+		pointBottomLeft.transform.position = positionBottomLeft;
+		pointBottomLeft.transform.parent = newColliderGameObject.transform;
+	
+		GameObject pointBottomRight = new GameObject();
+		pointBottomRight.name = newGameObjectName + " Bottom Right Point";
+		pointBottomRight.transform.position = positionBottomRight;
+		pointBottomRight.transform.parent = newColliderGameObject.transform;
+	
+		GameObject pointTopRight = new GameObject();
+		pointTopRight.name = newGameObjectName + " Top Right Point";
+		pointTopRight.transform.position = positionTopRight;
+		pointTopRight.transform.parent = newColliderGameObject.transform;
+	
+		GameObject pointTopLeft = new GameObject();
+		pointTopLeft.name = newGameObjectName + " Top Left Point";
+		pointTopLeft.transform.position = positionTopLeft;
+		pointTopLeft.transform.parent = newColliderGameObject.transform;
+	
+	
+		boxCollider.PolygonPoints = new Transform [] {
+			pointBottomLeft.transform,
+			pointBottomRight.transform,
+			pointTopRight.transform,
+			pointTopLeft.transform
+		};
 		
 		
 		return newColliderGameObject;
