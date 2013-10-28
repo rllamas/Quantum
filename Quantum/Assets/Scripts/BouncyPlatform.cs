99a5f1b5d747e1cd36c93da2001e2a34a243010d
SUMDude21 <roosterg21@gmail.com>
2013-10-27 20:33:37 -0500

    Working on new mechanics

---
+++ Quantum/Assets/Scripts/BouncyPlatform.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using FarseerPhysics.Dynamics;
+using FarseerPhysics.Dynamics.Contacts;
+using FVector2 = Microsoft.Xna.Framework.FVector2;
+
+public class BouncyPlatform : MonoBehaviour {
+	public Body body;
+	public float jumpFactor = 300f;
+	
+	
+	// Use this for initialization
+	void Start () {
+		body = GetComponent<FSBodyComponent>().PhysicsBody;
+		body.OnCollision += OnCollisionEvent;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	bool OnCollisionEvent(Fixture A, Fixture B, Contact contact) {
+		if (contact.IsTouching() && A.Body.UserTag == "Player") {
+			A.Body.ApplyLinearImpulse(new FVector2(0, jumpFactor));
+			Debug.Log("A: " + A.Body.LinearVelocity);
+		}
+		else if (contact.IsTouching() && B.Body.UserTag == "Player") {
+			B.Body.ApplyLinearImpulse(new FVector2(0, jumpFactor));
+			Debug.Log("B: " + B.Body.LinearVelocity);
+		}
+		return true;
+	}
+}
