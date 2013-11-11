313dfdc7952361ca3f5c1dd8585267a5e87f46e4
russelljahn <russelljahn@gmail.com>
2013-11-11 14:40:19 -0600

    Added dialogue to level 2

---
+++ Quantum/Assets/Scripts/Player Events/MysteriousHeights/MysteriousHeightsEvent001.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MysteriousHeightsEvent001 : PlayerEvent {
+	
+	
+	private bool alreadyActivated;
+	private bool leftTrigger;
+	
+	
+	public override void OnActivate(Player player) {
+		
+		if (alreadyActivated) {
+			return;	
+		}
+
+		base.OnActivate(player);
+
+		StartCoroutine( DoEvent() );
+		
+	}
+	
+	
+	
+	private IEnumerator DoEvent() {
+		
+		if (!player.IsDialogBoxHidden()) {
+			yield return new WaitForSeconds(1.0f);
+		}
+			
+		player.ShowDialogueBox();
+		player.SetDialogue("There's no plant here, huh...?");
+		
+		alreadyActivated = true;	
+		
+		yield return new WaitForSeconds(1.0f);
+		
+		while (!leftTrigger) {
+			yield return null;	
+		}
+		
+		yield return new WaitForSeconds(0.5f);
+		player.HideDialogueBox();
+		
+	}
+	
+	
+	
+	void OnTriggerExit() {
+		leftTrigger = true;
+	}
+	
+}
