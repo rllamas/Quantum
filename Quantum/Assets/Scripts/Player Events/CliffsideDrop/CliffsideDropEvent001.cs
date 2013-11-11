0f3f4c314563f2e8f0d282fb83e5d83d50e87bc6
russelljahn <russelljahn@gmail.com>
2013-11-11 14:53:50 -0600

    Added some dialogue to level 4.

---
+++ Quantum/Assets/Scripts/Player Events/CliffsideDrop/CliffsideDropEvent001.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CliffsideDropEvent001 : PlayerEvent {
+	
+	
+	private bool alreadyActivated;
+	private bool leftTrigger;
+	
+	
+	public override void OnActivate(Player player) {
+		
+		if (alreadyActivated) {
+			return;	
+		}
+
+		base.OnActivate(player);
+
+		StartCoroutine( DoEvent() );
+		
+	}
+	
+	
+	
+	private IEnumerator DoEvent() {
+		
+		if (!player.IsDialogBoxHidden()) {
+			yield return new WaitForSeconds(1.0f);
+		}
+		else {
+			yield return new WaitForSeconds(0.15f);
+		}
+			
+		player.ShowDialogueBox();
+		player.SetDialogue("WHHHEEEEEE!!!!");
+		
+		alreadyActivated = true;	
+		
+		
+		while (!leftTrigger) {
+			yield return null;	
+		}
+		
+		yield return new WaitForSeconds(0.25f);
+		player.HideDialogueBox();
+		
+	}
+	
+	
+	
+	void OnTriggerExit() {
+		leftTrigger = true;
+	}
+	
+}
