1499a9fd3b823273a5bf131ce6268c99911bc1f8
russelljahn <russelljahn@gmail.com>
2013-11-13 02:29:12 -0600

    Action text now disappears after staying the same for too long. It also now disappears when time traveling.

--- Quantum/Assets/Scripts/GUI/ActionGUI.cs
+++ Quantum/Assets/Scripts/GUI/ActionGUI.cs
@@ -6,19 +6,32 @@ public class ActionGUI : MonoBehaviour {
 	private Player player;
 	private tk2dTextMesh actionText;
 	
+	private float maxTimeUntilFade = 4.5f;
+	private float timeLeftUntilFade;
+	
+	private string actionTextLastFrame;
+
 
 	// Use this for initialization
 	void Start () {
 		
 		player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
 		actionText = this.transform.FindChild("Action Text").GetComponent<tk2dTextMesh>();
-		
+	
+		timeLeftUntilFade = maxTimeUntilFade;
 	}
 	
 	
 	
 	// Update is called once per frame
 	void Update () {
+		
+		actionTextLastFrame = actionText.text;
+		
+		if (Vortex.CurrentlyWarping()) {
+			actionText.text = "";
+			return;
+		}
 		
 		
 		/* Update content and look of action gui based on if player can action or not. */
@@ -56,9 +69,28 @@ public class ActionGUI : MonoBehaviour {
 			case (Player.ActionButtonStates.NONE):		
 				actionText.text = "";
 				break;
-			
 		}
-
 		
+		/* Redisplay the text if it has recently changed. */
+		if (!actionText.text.Equals(actionTextLastFrame)) {
+			actionText.gameObject.SetActive(true);	
+		}
+		
+		
+		/* Decrement the remaining time until you should hide the text if the text hasn't changed
+		 * since last frame. */
+		if (actionTextLastFrame.Equals(actionText.text)) {
+			timeLeftUntilFade = Mathf.Max(0, timeLeftUntilFade-Time.deltaTime);
+		}
+		else {
+			timeLeftUntilFade = maxTimeUntilFade;
+		}
+		
+		/* Hide text if it has been the same for too long. */
+		if (timeLeftUntilFade == 0f) {
+			timeLeftUntilFade = maxTimeUntilFade;
+			actionText.gameObject.SetActive(false);
+		}
+			
 	}
 }
