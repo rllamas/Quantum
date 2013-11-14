c2f55eaf4fa5789b4c3bd335c50572aebd63d21e
rllamas <ricardo.llamas11@yahoo.com>
2013-11-13 19:32:32 -0600

    Added a rudimentary pause prefab. GUI still needs work.

---
+++ Quantum/Assets/Scripts/GUI/Pause.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pause : MonoBehaviour {
+	GameObject pCam;
+	GameObject normCam;
+	
+	// Use this for initialization
+	void Start () {
+		pCam = transform.FindChild("PauseCamera").gameObject;
+		normCam = GameObject.Find("tk2dCamera").gameObject;
+		Debug.Log("Hello");
+		pCam.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Escape)) {
+			
+			pauseGame ();
+			changeCamera();
+		}
+	}
+	
+	void changeCamera(){
+		if(!pCam.activeSelf){
+			normCam.SetActive(false);
+			pCam.SetActive(true);
+		}else{
+			normCam.SetActive(true);
+			pCam.SetActive(false);
+		}
+	}
+	
+	
+	void pauseGame(){
+		if (Time.timeScale == 1.0F)
+            Time.timeScale = 0.0F;
+        else
+        	Time.timeScale = 1.0F;
+	}
+}
