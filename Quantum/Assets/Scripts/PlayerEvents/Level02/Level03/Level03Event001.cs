916cf32a7b947828dcb049586fd9273091fb6d4d
russelljahn <russelljahn@gmail.com>
2013-12-12 13:07:35 -0600

    Added dialogue event to 3rd level explaining the map/main menu.

---
+++ Quantum/Assets/Scripts/PlayerEvents/Level02/Level03/Level03Event001.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Level03Event001 : PlayerEvent {
+	
+	
+	private bool alreadyActivated;
+	private bool leftTrigger;
+	
+	
+	public override void OnActivate(Player player) {
+		
+		if (alreadyActivated) {
+			return;	
+		}
+
+		base.OnActivate(player);
+
+		StartCoroutine( DoEvent() );
+		
+	}
+	
+	
+	
+	private IEnumerator DoEvent() {
+		
+		if (!player.IsDialogBoxHidden()) {
+			yield return new WaitForSeconds(1.0f);
+		}
+			
+		player.ShowDialogueBox();
+		player.SetDialogue("Whew...\n\n    Sometimes I just need to ^cFF0F[ESC]^cFFFFape, take a break, and check out my map.");
+		
+		alreadyActivated = true;	
+		
+		yield return new WaitForSeconds(1.0f);
+		
+		while (!leftTrigger) {
+			yield return null;	
+		}
+		
+		yield return new WaitForSeconds(0.5f);
+		player.HideDialogueBox();
+		
+	}
+	
+	
+	
+	void OnTriggerExit() {
+		leftTrigger = true;
+	}
+	
+}
